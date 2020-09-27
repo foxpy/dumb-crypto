88450938ce43383d7b463fe12bdf78e593e94928
Murad <muradkarammaev@gmail.com>
2020-09-27 18:59:23 +0300

    implement Affine Caesar cipher

--- crypto/crypto.h
+++ crypto/crypto.h
@@ -3,3 +3,6 @@
 
 char* caesar_cipher_encrypt(const char* input_str, size_t key);
 char* caesar_cipher_decrypt(const char* input_str, size_t key);
+
+char* affine_caesar_cipher_encrypt(const char* input_str, size_t a, size_t b);
+char* affine_caesar_cipher_decrypt(const char* input_str, size_t a, size_t b);
