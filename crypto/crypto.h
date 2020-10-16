cc14338a79a66995d67113ecc0ec3f050f1ac005
Murad <muradkarammaev@gmail.com>
2020-10-16 11:39:56 +0300

    add affine Caesar GUI tool

--- crypto/crypto.h
+++ crypto/crypto.h
@@ -9,13 +9,13 @@ char* encryption_table_single_permutation_decrypt(char const* input_str, char co
 
 char* encryption_table_double_permutation_encrypt(
         char const* input_str,
-        size_t x, size_t const horizontal_permutation[static x],
-        size_t y, size_t const vertical_permutation[static y]
+        size_t x, size_t const* horizontal_permutation,
+        size_t y, size_t const* vertical_permutation
 );
 char* encryption_table_double_permutation_decrypt(
         char const* input_str,
-        size_t x, size_t const horizontal_permutation[static x],
-        size_t y, size_t const vertical_permutation[static y]
+        size_t x, size_t const* horizontal_permutation,
+        size_t y, size_t const* vertical_permutation
 );
 
 char* caesar_cipher_encrypt(char const* input_str, size_t key);
@@ -27,8 +27,8 @@ char* affine_caesar_cipher_decrypt(char const* input_str, size_t a, size_t b);
 char* caesar_keyword_cipher_encrypt(const char* input_str, size_t k, char const* key);
 char* caesar_keyword_cipher_decrypt(const char* input_str, size_t k, char const* key);
 
-char* magic_square_encrypt(char const* input_str, size_t len, size_t const square[static len]);
-char* magic_square_decrypt(char const* input_str, size_t len, size_t const square[static len]);
+char* magic_square_encrypt(char const* input_str, size_t len, size_t const* square);
+char* magic_square_decrypt(char const* input_str, size_t len, size_t const* square);
 
 char* trithemius_cipher_encrypt(char const* input_str, size_t x, size_t y, char const* key);
 char* trithemius_cipher_decrypt(char const* input_str, size_t x, size_t y, char const* key);
